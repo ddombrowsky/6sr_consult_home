788df34260204175c5ef7c61ba5a1e3da000e548
David Dombrowsky <davek@6thstreetradio.org>
2021-05-07 11:48:59 -0400

    added news link to homepage

--- build.sh
+++ build.sh
@@ -4,6 +4,13 @@ cd `dirname $0`
 
 set -ex
 
+rm -rf dist 6srticles/dist
+
+pushd 6srticles
+npm run build
+popd
+
 $(npm bin)/tsc -p src/api &&
 $(npm bin)/ng build --prod &&
 cp ./static/* ./dist/ussr/
+cp -r ./6srticles/dist ./dist/ussr/a
